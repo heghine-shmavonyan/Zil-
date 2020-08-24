e6b0a22712372cd40d79f8a27c2cac1bea338f6f
Heghine Shmavonyan <heghine.shmavonyan@gmail.com>
2020-08-24 18:59:31 +0400

    homework

---
+++ Lesson 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*  * ****  ****     *  *  *  *     * **** ");    
+            Console.WriteLine("*  * *     *    *   *  *  *  * *   * *");   
+            Console.WriteLine("**** ****  *  ****  ****  *  *  *  * ****");
+            Console.WriteLine("*  * *     *    *   *  *  *  *   * * *");
+            Console.WriteLine("*  * ****  * *  *   *  *  *  *    ** ****  ");
+            Console.ReadKey();
+
+        }
+    }
+}
