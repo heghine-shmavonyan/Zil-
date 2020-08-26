68d6e4d923088262757b322a52958e0d1299c56b
Heghine Shmavonyan <heghine.shmavonyan@gmail.com>
2020-08-26 12:33:50 +0400

    Level 2

---
+++ Lesson 2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a;
+            a = 4;
+            byte x =5;
+            sbyte y = -5;
+            short m = 3000;
+            ushort n = 6000;
+            int i = 2000000;
+            uint l = 4000000;
+            long o = 9000000000000000;
+            ulong s = 19000000000000000;
+            float h = 2.402823f;
+            double w = 50000;
+            char j = '(' ;
+            bool r = true;
+
+            String name = "Heghine";
+            String surname = "Shmavonyan";
+
+
+            Console.WriteLine(name + " " + surname);
+            Console.ReadKey();
+
+
+        }   
+    }
+}
