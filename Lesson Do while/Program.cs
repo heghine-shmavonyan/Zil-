424b2b1914f56a96cf2b9f624b44eda87d0901c9
Heghine Shmavonyan <heghine.shmavonyan@gmail.com>
2020-09-09 18:32:36 +0400

    21,22

---
+++ Lesson Do while/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_Do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Խնդիր_21:Արտածել տրված թվի բոլոր բաժանարարները։
+            //Խնդիր_22:Հաշվել տրված թվի բոլոր բաժանարարների գումարը։
+
+            int num = int.Parse(Console.ReadLine());
+            int num1 = 2;
+            int sum = 0;
+   
+            while (true)
+                {
+                if (num==1)
+                {
+                    break;
+                }
+                if (num%num1==0)
+                {
+                    num = num / num1;
+                    Console.WriteLine(num1);
+                    sum = sum + num1;
+                }
+                else
+                {
+                    num1++;
+                }
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+
+        }
+    }
+}
--- arrays/Program.cs
+++ arrays/Program.cs
@@ -7,22 +7,21 @@ using System.Threading.Tasks;
 namespace arrays
 {
     class Program
-    {
+    {   
         static void Main(string[] args)
-        {
+        { // 14 Ներածել n թիվը։ Արտածել yes, եթե այն հավասար է իր թվանշանների գումարի կրկնապատիկին, no՝ հակառակ դեպքում։
 
 
-            int [] nums  = new int[] {5,4,3,8,6,9,7};
-            for (int i = 0; i <= nums.Length; i++)
+
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while (num > 0)
             {
-                if (nums[i] * nums[i] < nums[nums.Length-i] * nums[nums.Length - i])
-
-                {
-                    Console.WriteLine(nums[i]);
-                }
-
+                sum += num % 10;
+                num /= 10;
             }
-
+            Console.WriteLine(sum);
             Console.ReadKey();
 
         }
