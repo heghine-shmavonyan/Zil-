5f3a88bc10bb6ea3385b8438b535ef8c02c115a7
Heghine Shmavonyan <heghine.shmavonyan@gmail.com>
2020-09-09 18:51:52 +0400

    21,22,23

--- Lesson Do while/Program.cs
+++ Lesson Do while/Program.cs
@@ -12,29 +12,35 @@ namespace Lesson_Do_while
         {
             //Խնդիր_21:Արտածել տրված թվի բոլոր բաժանարարները։
             //Խնդիր_22:Հաշվել տրված թվի բոլոր բաժանարարների գումարը։
+            //Խնդիր_23:Հաշվել տրված թվի բոլոր բաժանարարների գումարի և դրանց քանակի քանորդը։
 
             int num = int.Parse(Console.ReadLine());
             int num1 = 2;
             int sum = 0;
+            double count = 0;
+
    
             while (true)
                 {
-                if (num==1)
+                if (num==1 || num ==0)
                 {
-                    break;
+                    return;
                 }
                 if (num%num1==0)
                 {
                     num = num / num1;
                     Console.WriteLine(num1);
                     sum = sum + num1;
+                    count++;
                 }
                 else
                 {
                     num1++;
                 }
+
             }
             Console.WriteLine(sum);
+            Console.WriteLine(sum/count);
             Console.ReadKey();
 
         }
